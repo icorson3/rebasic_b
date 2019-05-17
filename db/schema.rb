1804340f5baedee5779fca79c895f5f6279bf2f1
Trevor Nodland <trevor.nodland@gmail.com>
2019-05-17 09:27:52 -0600

    adds migration to create item table and adds items to controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_17_152629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.integer "name"
+  end
 
 end
